236af1d43c5113b3889ec5edb86ef7e87c24fda5
mrhorin <u0813mb@gmail.com>
2017-05-14 18:39:52 +0900

    レスを改行表示してリンクを規定ブラウザで開けるように

--- src/jsx/component/app/post.jsx
+++ src/jsx/component/app/post.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { shell } from 'electron'
 
 export default class Post extends React.Component {
 
   constructor(props) {
     super(props)
+  }
+  
+  parseBody = (text) => {
+    return this.replaceUrl(text)
+  }
+
+  // URLを置換  
+  replaceUrl = (text) => {
+    var elements = []
+    const ptn = /h?ttps?:\/\/[-_\.!~*'()a-zA-Z0-9;\/?:@&=+$,%#¥]+/i
+    // URLをセパレータで区切る
+    text = text.replace(ptn, (url) => { return `<>${url}<>` })
+    text.split("<>").map((element, index) => {
+      if (element.match(ptn)) {
+        // URLをリンク化
+        elements.push(<a key={index} onClick={() => { this.openBrowser(element) }}>{element}</a>)
+      } else {
+        elements.push(this.replaceBr(element))
+      }
+    })
+    return elements
+  }
+
+  // 改行タグを置換  
+  replaceBr = (text) => {
+    return text.split("<br>").map((line, index) => {
+      return (
+        <div className="post-body-line" key={index}>{line}</div>
+      )
+    })
+  }
+
+  // 規定ブラウザで開く
+  openBrowser = (url) => {
+    shell.openExternal(url)
   }
 
   render() {
@@ -20,7 +56,7 @@ export default class Post extends React.Component {
           <span className="post-id">{this.props.post.id}</span>
         </div>
         <div className="post-body">
-          {this.props.post.body}
+          {this.parseBody(this.props.post.body)}
         </div>
       </div>
     )
