daf73e7cae77e049e575bd50b1f01ea2d9163b53
mrhorin <u0813mb@gmail.com>
2019-06-07 21:06:14 +0900

    PostTooltipを自動調整するように修正

--- src/jsx/component/app/post_tooltip.jsx
+++ src/jsx/component/app/post_tooltip.jsx
@@ -4,11 +4,47 @@ export default class PostTooltip extends React.Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      style: {
+        visibility: 'hidden',
+        bottom: '-20px',
+        left: '0px'
+      }
+    }
+  }
+
+  get windowSize() {
+    var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    w = w.innerWidth || e.clientWidth || g.clientWidth,
+    h = w.innerHeight|| e.clientHeight|| g.clientHeight;
+    return { width: w, height: h }
+  }
+
+  componentDidMount() {
+    // tooltipの位置を調整
+    let style = { visibility: 'visible', bottom: '-20px', left: '0px' }
+    let rect = this.refs.tooltip.getBoundingClientRect()
+    let windowSize = this.windowSize
+    // 右がはみ出ているか
+    if ((rect.x + rect.width) > windowSize.width) {
+      // tooltipのサイズがwindowサイズより小さいか
+      if (rect.width < windowSize.width) {
+        // 右詰めで表示
+        style['left'] = `-${Math.round(rect.width - (windowSize.width - rect.x))}px`
+      } else {
+        // 左詰めで表示
+        style['left'] = `-${Math.round(rect.x)}px`
+      }
+    }
+    this.setState({ style: style })
   }
 
   render() {
     return (
-      <div className="post-tooltip" onMouseLeave={this.props.hideTooltip}>
+      <div ref="tooltip" className="post-tooltip" style={this.state.style} onMouseLeave={this.props.hideTooltip}>
           {this.props.component}
       </div>
     )
