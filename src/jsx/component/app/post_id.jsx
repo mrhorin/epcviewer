126c0f394528d08fa08c59ee904998be077a3e43
mrhorin <u0813mb@gmail.com>
2019-06-21 16:06:59 +0900

    IDに'ID:'が含まれる場合は削除して表示するように修正

--- src/jsx/component/app/post_id.jsx
+++ src/jsx/component/app/post_id.jsx
@@ -10,6 +10,10 @@ export default class PostId extends React.Component {
     this.state = { showTooltip: false, tooltipComponent: "" }
   }
 
+  get id() {
+    return this.props.id.replace(/^ID:/, "")
+  }
+
   get idCounter() {
     let counter = this.props.getIdCounter(this.props.no)
     let count = counter.findIndex((value, index) => {
@@ -23,19 +27,19 @@ export default class PostId extends React.Component {
       let posts = this.props.getIdCounter(this.props.no).map((no, index) => {
         let post = this.props.getPost(no)
         return (
-          <Post key={index} no={no} post={post} getPost={this.props.getPost} getIdCounter={this.props.getIdCounter} />          
+          <Post key={index} no={no} post={post} getPost={this.props.getPost} getIdCounter={this.props.getIdCounter} />
         )
       })
       this.setState({
         showTooltip: true,
         tooltipComponent: posts
-      })      
+      })
     }
   }
 
   hideTooltip = () => {
     if (this.state.showTooltip) {
-      this.setState({ showTooltip: false, tooltipComponent: '' })      
+      this.setState({ showTooltip: false, tooltipComponent: '' })
     }
   }
 
@@ -55,7 +59,7 @@ export default class PostId extends React.Component {
         <div className={extractClass} onMouseOver={this.showTooltip}>
           {tooltip}
         </div>
-        <div className="post-id-uid">{this.props.id}</div>
+        <div className="post-id-uid">{this.id}</div>
         <div className="post-id-counter">
           <div className="post-id-counter-count">{idCounter.count}</div>
           <div className="post-id-counter-total">{idCounter.total}</div>
